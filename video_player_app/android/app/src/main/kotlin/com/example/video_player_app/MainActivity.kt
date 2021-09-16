9826adf7d79b8056a2bf45d737421e81f2b8248b
OwenLee <2924354040@qq.com>
2021-09-16 22:38:12 +0800

    create video player app

---
+++ video_player_app/android/app/src/main/kotlin/com/example/video_player_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_player_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ video_player_app/lib/colors.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class AppColor{
+  static  Color homePageBackground=const Color(0xFFfbfcff);
+  static  Color gradientFirst=const Color(0xff0f17ad);
+  static  Color gradientSecond=const Color(0xFF6985e8);
+  static  Color homePageTitle=const Color(0xFF302f51);
+  static  Color homePageSubtitle=const Color(0xFF414160);
+  static  Color homePageDetail=const Color(0xFF6588f4);
+  static  Color homePageIcons=const Color(0xFF3b3c5c);
+  static  Color homePageContainerTextSmall=const Color(0xFFf4f5fd);
+  static  Color homePageContainerTextBig=const Color(0xFFffffff);
+  static Color homePagePlanColor=const Color(0xFFa2a2b1);
+  static Color secondPageTopIconColor= const Color(0xFFb7bce8);
+  static Color secondPageTitleColor= const Color(0xFFfefeff);
+  static Color secondPageContainerGradient1stColor= const Color(0xFF5564d8);
+  static Color secondPageContainerGradient2ndColor= const Color(0xFF6279dc);
+  static Color secondPageIconColor= const Color(0xFFfafafe);
+  static Color loopColor = const Color(0xFF6d8dea);
+  static Color setsColor = const Color(0xFF9999a9);
+  static Color circuitsColor = const Color(0xFF2f2f51);
+}
