8e808040de1e44cdfac8920b8c4347fcc2a09c0e
OwenLee <2924354040@qq.com>
2021-11-06 01:53:41 +0800

    修复拼写错误

--- flutter_in_action_codes/lib/layout_widgets/layout_builder_demo_page.dart
+++ flutter_in_action_codes/lib/layout_widgets/layout_builder_demo_page.dart
@@ -100,7 +100,7 @@ class LayoutLogPrinter<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (BuildContext context, Bo  xConstraints constraints) {
+        builder: (BuildContext context, BoxConstraints constraints) {
       debugPrint("${tag ?? key ?? child} : $constraints");
       return child;
     });
